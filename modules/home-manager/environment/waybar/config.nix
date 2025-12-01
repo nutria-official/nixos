d3ae71dcf4bb5209357cf0c3df56058b62716b96
nutria-official <nutria.official@proton.me>
2025-12-01 09:35:45 +0100

    update: New waybar config, taken from jamespness.

--- modules/home-manager/environment/waybar/config.nix
+++ modules/home-manager/environment/waybar/config.nix
@@ -4,25 +4,101 @@
     spacing = 5;
     layer = "top";
     position = "top";
+
     modules-center = [
       "clock"
     ];
 
     modules-right = [
+      "network"
+      "bluetooth"
+      "backlight"
       "pulseaudio"
       "battery"
     ];
 
     modules-left = [
-      "custom/spider"
       "hyprland/workspaces"
     ];
+
+    backlight = {
+      format = "{icon}{percent}%";
+      format-icons = [
+        "󰃜 "
+        "󰃛 "
+        "󰃚 "
+      ];
+    };
+
+    bluetooth = {
+      format = "󰂯 {status} - Devices: {num_connections}";
+      format-disabled = "󰂲 {status}";
+      format-connected = "󰥰  {status} - Devices: {num_connections}";
+      format-connected-battery = "󰥰  status - {device_battery_percentage}% - Devices: {num_connections}";
+      on-click = "blueman-manager";
+    };
+
+    network = {
+      interval = 1;
+      family = "ipv4-6";
+      format = "{icon}{essid}";
+      format-alt = "{icon}{essid}";
+      format-wifi = "{icon}{essid} -  {bandwidthDownBits}   {bandwidthUpBits} ";
+      format-ethernet = "󰈀 {essid} -  {bandwidthDownBits}   {bandwidthUpBits} ";
+      format-disconnected = "󰤭 - No WIFI";
+      format-icons = [
+        "󰤯 "
+        "󰤟 "
+        "󰤢 "
+        "󰤥 "
+        "󰤨 "
+      ];
+    };
+
+    pulseaudio = {
+      format = "{icon} {volume}%";
+      format-bluetooth = "󰋎 {volume}%";
+      format-muted = "";
+      format-icons = [
+        "󰕿"
+        "󰖀"
+        "󰕾"
+      ];
+    };
+
+    clock = {
+      format = "{:%H:%M}";
+      format-alt = "{:%R - %A, %B %d, %Y}";
+      tooltip-format = "<tt><small>{calendar}</small></tt>";
+
+      calendar = {
+        mode = "year";
+        mode-mon-col = 3;
+        weeks-pos = "right";
+
+        format = {
+          months = "<span color='#ffead3' font-family = 'Comic Mono'><b>{}</b></span>";
+          days = "<span color='#ecc6d9' font-family = 'Comic Mono'><b>{}</b></span>";
+          weeks = "<span color='#99ffdd' font-family = 'Comic Mono'><b>W{}</b></span>";
+          weekdays = "<span color='#ffcc66' font-family = 'Comic Mono'><b>{}</b></span>";
+          today = "<span color='#ff6699' font-family = 'Comic Mono'><b><u>{}</u></b></span>";
+        };
+      };
+
+      actions = {
+        on-click-right = "mode";
+        on-click-forward = "tz-up";
+        on-click-backward = "tz-down";
+        on-scroll-up = "shift-up";
+        on-scroll-down = "shift-down";
+      };
+    };
 
     battery = {
       format = "{capacity}% {icon}";
       states = {
-        warning = 40;
-        critical = 20;
+        warning = 20;
+        critical = 10;
       };
       format-plugged = "{capacity}% 󰂄";
       format-icons = [
@@ -37,33 +113,7 @@
         "󰂂"
         "󰁹"
       ];
-      tooltip = false;
-    };
-
-    pulseaudio = {
-      format = "{volume}% {icon}";
-      format-muted = "";
-      format-icons = [
-        "\uf026"
-        "\uf027"
-        "\uf028"
-      ];
-      tooltip = false;
-    };
-
-    "custom/spider" = {
-      format = ''\uef10'';
-      on-click = "hyprsunset -t 4000";
-      tooltip = false;
-    };
-
-    "hyprland/workspaces" = {
-      format = "{icon}";
-      format-icons = {
-        active = "";
-        default = "";
-      };
-      move-to-monitor = true;
+      tooltip = true;
     };
   };
 }
