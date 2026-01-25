8710894dae2c73d103be53e66d917e7a2b683082
nutria-official <nutria.official@proton.me>
2026-01-25 21:55:16 +0100

    update: Nix flake horrible new syntax fix.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "nixpkgs/nixos-unstable";
     };
     home-manager = {
       url = "github:nix-community/home-manager/";
